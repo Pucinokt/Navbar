ba4f71cc387aff0d37b9bf656246e1b74ee822fb
Pucinokt <pucinokt@hotmail.com>
2022-05-31 23:09:21 +0200

    update
    update

---
+++ NAVBAR - LOGO LEFT/script.js
@@ -0,0 +1,16 @@
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
